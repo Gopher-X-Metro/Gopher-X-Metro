fe6a540aea8e4d2ff20816d10f3232f895da6e81
CubeToPlay <hwvxyeej@gmail.com>
2024-02-22 17:10:15 -0600

    cleaned up some of the code with Element's abstract class

--- src/frontend/elements/Stop.ts
+++ src/frontend/elements/Stop.ts
@@ -1,18 +1,19 @@
-class Stop {
+import Element from "./Element.ts";
+
+class Stop extends Element {
 
     /* Public */
 
     /**
      * Stop Constructor
-     * @param routeId route ID the stop belongs to
      * @param stopId ID of the stop
      * @param color color of the stop
      * @param location location of the stop
      * @param map map the stop displays on
      */
-    constructor(routeId: string, stopId: string, color: string, location: google.maps.LatLng, map: google.maps.Map) {
-        this.stopId = stopId;
-        this.routeId = routeId
+    constructor(stopId: string, color: string, location: google.maps.LatLng, map: google.maps.Map) {
+        super(stopId, color, map);
+
         this.location = location;
 
         this.stopTimes = new Map<string, string | undefined>();
@@ -32,10 +33,6 @@ class Stop {
      */
     public getMarker() : google.maps.Circle { return this.marker; }
     /**
-     * Gets the ID of the stop
-     */
-    public getStopId() : string { return this.stopId; }
-    /**
      * Gets the stop times hash
      */
     public getStopTimes() : Map<string, string | undefined> { return this.stopTimes; }
@@ -47,9 +44,6 @@ class Stop {
     public addStopTime(vehicleId: string, time: string | undefined) { this.stopTimes.set(vehicleId, time); }
 
     /* Private */
-
-    private stopId: string;
-    private routeId: string;
 
     private stopTimes: Map<string, string | undefined>;
 
