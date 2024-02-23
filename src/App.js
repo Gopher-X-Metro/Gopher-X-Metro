85fdf4d21da3b2a2a104dcd70080e0d97e4af64e
Avieagle <79376577+Avieagle@users.noreply.github.com>
2024-02-23 13:50:30 -0600

    Updated NavBar and Created Scheduling Page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import React from 'react';
-import Map from './frontend/Map.tsx'
-import NavBar from './frontend/NavBar.tsx';
+import React from "react";
+import Map from "./frontend/Map.tsx";
+import NavBar from "./frontend/NavBar.tsx";
 
-import "./styles.css"
+import "./styles.css";
+import Main from "./frontend/Main.tsx";
 
 export default function App() {
   return (
     <>
       <NavBar />
-      <Map />
+      <Main />
     </>
   );
 }
