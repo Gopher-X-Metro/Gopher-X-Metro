70d8be2ae7449f83547685c472b9ece7e428a1e2
AndyP <malicioussoap@gmail.com>
2024-03-21 13:23:06 -0500

    hid errors for later use

--- src/backend/Data.ts
+++ src/backend/Data.ts
@@ -95,12 +95,12 @@ namespace Data {
             if (response.ok) {
                 return response.json();
             } else {
-                const responseBodyText = await response.text(); // Get the response body as text
-                console.log(`Data fetching encountered status code ${response.status} with University Data. Response Body: ${responseBodyText}`);
+                //const responseBodyText = await response.text(); // Get the response body as text
+                //console.log(`Data fetching encountered status code ${response.status} with University Data. Response Body: ${responseBodyText}`);
                 return null;
             }
         } catch (error) {
-            console.error('Error fetching data:', error);
+            //console.error('Error fetching data:', error);
             return null;
         }
     }
@@ -114,13 +114,13 @@ namespace Data {
             if (response.ok) {
                 return GtfsRealtimeBindings.transit_realtime.FeedMessage.decode(new Uint8Array(await response.arrayBuffer()));
             } else {
-                const responseBodyText = await response.text(); // Get the response body as text
-                console.log(`Data fetching encountered status code ${response.status} with Vehicle Position Data. Response Body: ${responseBodyText}`);
+                //const responseBodyText = await response.text(); // Get the response body as text
+                //console.log(`Data fetching encountered status code ${response.status} with Vehicle Position Data. Response Body: ${responseBodyText}`);
                 return null;
             }
         }
         catch (error) {
-            console.error('Error fetching data:', error);
+            //console.error('Error fetching data:', error);
             return null;
         }
     }
@@ -134,13 +134,13 @@ namespace Data {
             if (response.ok) {
                 return GtfsRealtimeBindings.transit_realtime.FeedMessage.decode(new Uint8Array(await response.arrayBuffer()));
             } else {
-                const responseBodyText = await response.text(); // Get the response body as text
-                console.log(`Data fetching encountered status code ${response.status} with Trip Updates Data. Response Body: ${responseBodyText}`);
+                //const responseBodyText = await response.text(); // Get the response body as text
+                //console.log(`Data fetching encountered status code ${response.status} with Trip Updates Data. Response Body: ${responseBodyText}`);
                 return null; 
             }
         }
         catch (error) {
-            console.error('Error fetching data:', error);
+            //console.error('Error fetching data:', error);
             return null;
         }
     }
