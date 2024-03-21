55bdce11289c33349530fc163498b469a99b986b
Kennedy Johnson <joh20327@umn.edu>
2024-03-20 19:31:22 -0500

    made stop buttons

--- src/frontend/elements/Stop.ts
+++ src/frontend/elements/Stop.ts
@@ -1,3 +1,4 @@
+//import Resources from "../../backend/Resources.ts";
 class Stop {
 
     /* Public */
@@ -19,14 +20,54 @@ class Stop {
 
         this.marker = new window.google.maps.Circle({
             fillColor: color,
-            fillOpacity: 0,
+            fillOpacity: 100,
             strokeColor: color,
             center: this.location,
-            radius: 10,
+            radius: 5,
             clickable: true,
             map: map
-        })
+        });
+        //const Circle_color = Resources.getColor(this.routeId);
+        //const tripID = Resources.getTripIds(this.routeId).toString();
+        const contentString = "Route ID: " + this.routeId;
+        
+        const infoWindow = new window.google.maps.InfoWindow({
+            ariaLabel: contentString,
+            content: contentString,
+            maxWidth: 200, // Set the maximum width of the info window
+            //maxHeight: 200, // Set the maximum height of the info window
+        });
+        infoWindow.setPosition(location);
+        let infoWindowOpen = false; // Flag to indicate whether the info window is open
+        this.marker.addListener('click', () => {
+            if (infoWindowOpen) {
+                infoWindow.close();
+                infoWindowOpen = false; // Set flag to indicate info window is closed
+            } else {
+                infoWindow.open(map, this.marker);
+                infoWindowOpen = true; // Set flag to indicate info window is open
+            }
+            // Calculate the pixel offset to position the info window above the marker
+            const markerPosition = location;
+            const pixelOffset = new window.google.maps.Size(0, -20); // Adjust the Y offset as needed
+
+            // Open the info window at the clicked marker's position with custom options
+            infoWindow.setOptions({
+                position: markerPosition,
+                pixelOffset: pixelOffset,
+            });
+        });
+        this.infoWindow = infoWindow; // jesus
     }
+    public closeInfoWindow() {
+        if(this.infoWindow) {
+            this.infoWindow.close();
+        }
+    }
+    /**
+     * Gets the info window object on the map
+     */
+    public getInfoWindow() : google.maps.InfoWindow { return this.infoWindow; }
     /**
      * Gets the marker object on the map
      */
@@ -50,9 +91,8 @@ class Stop {
 
     private stopId: string;
     private routeId: string;
-
+    public infoWindow: google.maps.InfoWindow;
     private stopTimes: Map<string, string | undefined>;
-
     private location: google.maps.LatLng;
     private marker: google.maps.Circle;
 }
