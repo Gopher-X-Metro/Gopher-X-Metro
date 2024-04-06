3ed45299e0bbc726dbc6a764fc41798bc3d74a23
AndyP <malicioussoap@gmail.com>
2024-04-05 19:40:12 -0500

    added public key and firebase stuff

---
+++ src/frontend/Map/Notify.ts
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getMessaging, getToken } from "firebase/messaging";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB3o9UUOJ2xuV0zJr1zUWdWuDJuKeu_oHA",
+  authDomain: "gopher-x-metro.firebaseapp.com",
+  projectId: "gopher-x-metro",
+  storageBucket: "gopher-x-metro.appspot.com",
+  messagingSenderId: "426388269171",
+  appId: "1:426388269171:web:0301fc723626584fd42206",
+  measurementId: "G-9D0HN8MPVE"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const messaging = getMessaging();
+// Add the public key generated from the console here.
+getToken(messaging, {vapidKey: "BFHVjy-c13qU-ihnLFzru2kguRerHYpJ7CR-ADkDBXTwJivFXm5vHEND0F8UTfxfVkFJjPWh8iNhf1S9P2UE4u0"});
