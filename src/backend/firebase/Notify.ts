ca76eef4c4b6ae57131e306ea50eff65f5ecb9d2
AndyP <malicioussoap@gmail.com>
2024-04-05 19:45:03 -0500

    pasted firebase docs as is

--- src/backend/firebase/Notify.ts
+++ src/backend/firebase/Notify.ts
@@ -30,3 +30,22 @@ function requestPermission() {
   Notification.requestPermission().then((permission) => {
     if (permission === 'granted') {
       console.log('Notification permission granted.');
+
+
+
+    // Get registration token. Initially this makes a network call, once retrieved
+// subsequent calls to getToken will return from cache.
+const messaging = getMessaging();
+getToken(messaging, { vapidKey: '<YOUR_PUBLIC_VAPID_KEY_HERE>' }).then((currentToken) => {
+  if (currentToken) {
+    // Send the token to your server and update the UI if necessary
+    // ...
+  } else {
+    // Show permission request UI
+    console.log('No registration token available. Request permission to generate one.');
+    // ...
+  }
+}).catch((err) => {
+  console.log('An error occurred while retrieving token. ', err);
+  // ...
+});
