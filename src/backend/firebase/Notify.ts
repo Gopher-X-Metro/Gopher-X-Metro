cef8856c6f49df1f3ccc8e91d0c6a3896137f52f
AndyP <malicioussoap@gmail.com>
2024-04-05 20:11:22 -0500

    moved service account + added test code

--- src/backend/firebase/Notify.ts
+++ src/backend/firebase/Notify.ts
@@ -14,7 +14,7 @@ const firebaseConfig = {
   storageBucket: "gopher-x-metro.appspot.com",
   messagingSenderId: "426388269171",
   appId: "1:426388269171:web:0301fc723626584fd42206",
-  measurementId: "G-9D0HN8MPVE"
+  measurementId: "G-9D0HN8MPVE",
 };
 
 // Initialize Firebase
@@ -22,31 +22,42 @@ const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const messaging = getMessaging();
 // Add the public key generated from the console here.
-getToken(messaging, { vapidKey: "BFHVjy-c13qU-ihnLFzru2kguRerHYpJ7CR-ADkDBXTwJivFXm5vHEND0F8UTfxfVkFJjPWh8iNhf1S9P2UE4u0" });
-
+getToken(messaging, {
+  vapidKey:
+    "BFHVjy-c13qU-ihnLFzru2kguRerHYpJ7CR-ADkDBXTwJivFXm5vHEND0F8UTfxfVkFJjPWh8iNhf1S9P2UE4u0",
+});
 
 function requestPermission() {
-  console.log('Requesting permission...');
+  console.log("Requesting permission...");
   Notification.requestPermission().then((permission) => {
-    if (permission === 'granted') {
-      console.log('Notification permission granted.');
+    if (permission === "granted") {
+      console.log("Notification permission granted.");
     }
   });
 }
 
-// Get registration token. Initially this makes a network call, once retrieved
-// subsequent calls to getToken will return from cache.
-getToken(messaging, { vapidKey: 'BFHVjy-c13qU-ihnLFzru2kguRerHYpJ7CR-ADkDBXTwJivFXm5vHEND0F8UTfxfVkFJjPWh8iNhf1S9P2UE4u0' })
-  .then((currentToken) => {
-    if (currentToken) {
-      // Send the token to your server and update the UI if necessary
+export function test() {
+  requestPermission();
+
+  // Get registration token. Initially this makes a network call, once retrieved
+  // subsequent calls to getToken will return from cache.
+  getToken(messaging, {
+    vapidKey:
+      "BFHVjy-c13qU-ihnLFzru2kguRerHYpJ7CR-ADkDBXTwJivFXm5vHEND0F8UTfxfVkFJjPWh8iNhf1S9P2UE4u0",
+  })
+    .then((currentToken) => {
+      if (currentToken) {
+        // Send the token to your server and update the UI if necessary
+        // ...
+      } else {
+        // Show permission request UI
+        console.log(
+          "No registration token available. Request permission to generate one.",
+        );
+        // ...
+      }
+    }).catch((err) => {
+      console.log("An error occurred while retrieving token. ", err);
       // ...
-    } else {
-      // Show permission request UI
-      console.log('No registration token available. Request permission to generate one.');
-      // ...
-    }
-  }).catch((err) => {
-    console.log('An error occurred while retrieving token. ', err);
-    // ...
-  });
+    });
+}
