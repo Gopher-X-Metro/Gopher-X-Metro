c7ad1205eb65110354993b9baf17235febf3127d
CubeToPlay <86120028+CubeToPlay@users.noreply.github.com>
2024-04-17 19:27:59 -0500

    Cleaned some of Search_feature.tsx

--- src/frontend/NavBar/components/Search_feature.tsx
+++ src/frontend/NavBar/components/Search_feature.tsx
@@ -1,25 +1,22 @@
 import React, {useState} from 'react';
 
 import URL from '../../../backend/URL.ts';
-
 import Routes from '../../Map/Routes.ts';
 import Vehicles from '../../Map/Vehicles.ts';
 
-
-var count = 0;
-var old_route: string;
-function search_route(){
+let count = 0;
+let old_route: string;
+export default function search_route(){
 
     let routeInput = document.getElementById("search_route") as HTMLInputElement | null;
     
     if (routeInput) {
-        
-        var bus_route: string = routeInput.value;
+        let bus_route: string = routeInput.value;
         console.log(bus_route + "current");
         
-        if(isNaN(parseInt(bus_route))){
+        if(isNaN(parseInt(bus_route))) {
             console.log("error");
-        }else{
+        } else {
             if(!URL.getRoutes().has(bus_route) && count == 0){
                 console.log(bus_route + "1st if");
                 URL.addRoute(bus_route);
@@ -29,8 +26,7 @@ function search_route(){
                 old_route = bus_route;
                 console.log(old_route + "old");
                 
-            }else if(URL.getRoutes().has(old_route) && count == 1){
-                 
+            } else if(URL.getRoutes().has(old_route) && count == 1) {
                  console.log("OLDDD" + old_route);
                  console.log("count second if" + count);
                 // URL.addRoute(bus_route);
@@ -45,13 +41,7 @@ function search_route(){
 
          Routes.refresh();
          Vehicles.refresh();
-       
-        
-
-    }else{
+    } else {
         console.log("error input is NULL");
     }
-      
-  }//ends func
-
-  export default search_route;
+}
