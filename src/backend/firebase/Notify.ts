4f6c6dcd569b15bf0d11bc28f0f81a4a9af5af29
AndyP <malicioussoap@gmail.com>
2024-04-18 19:15:33 -0500

    had to update import???

--- src/backend/firebase/Notify.ts
+++ src/backend/firebase/Notify.ts
@@ -1,37 +1,27 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getMessaging, getToken, isSupported } from "firebase/messaging";
-
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyB3o9UUOJ2xuV0zJr1zUWdWuDJuKeu_oHA",
-  authDomain: "gopher-x-metro.firebaseapp.com",
-  projectId: "gopher-x-metro",
-  storageBucket: "gopher-x-metro.appspot.com",
-  messagingSenderId: "426388269171",
-  appId: "1:426388269171:web:0301fc723626584fd42206",
-  measurementId: "G-9D0HN8MPVE",
-};
-
-
+import {deleteToken,getMessaging,getToken,MessagePayload,onMessage,isSupported} from "firebase/messaging";
+import { firebaseConfig, vapidKey } from "./Config.ts";
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+const messaging = getMessaging(app);
 
+onMessage(messaging, (payload) => {
+  console.log("Message received. ", payload);
+});
 
 async function requestPermission() {
   console.log("Requesting permission...");
   Notification.requestPermission().then((permission) => {
+
     if (permission === "granted") {
       console.log("Notification permission granted.");
-      return true; 
+      return true;
     }
+
   });
   return false;
 }
@@ -44,20 +34,16 @@ export function test() {
     return;
   }
 
-
   const messaging = getMessaging();
-
 
   // Get registration token. Initially this makes a network call, once retrieved
   // subsequent calls to getToken will return from cache.
   getToken(messaging, {
-    vapidKey:
-      "BFHVjy-c13qU-ihnLFzru2kguRerHYpJ7CR-ADkDBXTwJivFXm5vHEND0F8UTfxfVkFJjPWh8iNhf1S9P2UE4u0",
+    vapidKey,
   })
     .then((currentToken) => {
       if (currentToken) {
-
-        console.log("Token: ", currentToken); 
+        console.log("Token: ", currentToken);
         // Send the token to your server and update the UI if necessary
         // ...
       } else {
