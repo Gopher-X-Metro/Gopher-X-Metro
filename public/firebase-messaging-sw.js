e94b0334e76275525f4767f6c1e4617b7ccff9be
AndyP <malicioussoap@gmail.com>
2024-04-18 19:17:29 -0500

    reformat to match original file

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -3,6 +3,7 @@
 // If you do not serve/host your project using Firebase Hosting see https://firebase.google.com/docs/web/setup
 importScripts('https://www.gstatic.com/firebasejs/9.2.0/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/9.2.0/firebase-messaging-compat.js');
+importScripts('/__/firebase/init.js');
 
 
 const messaging = firebase.messaging();
