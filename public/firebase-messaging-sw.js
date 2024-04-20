7910c31fb7499cfbb7333db483630e8bdbde8b50
AndyP <malicioussoap@gmail.com>
2024-04-20 14:48:57 -0500

    using new import statements

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,9 +1,14 @@
 // Import and configure the Firebase SDK
 // These scripts are made available when the app is served or deployed on Firebase Hosting
 // If you do not serve/host your project using Firebase Hosting see https://firebase.google.com/docs/web/setup
-importScripts('https://www.gstatic.com/firebasejs/9.2.0/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/9.2.0/firebase-messaging-compat.js');
-importScripts('/__/firebase/init.js');
+// importScripts('/__/firebase/10.11.0/firebase-app-compat.js');
+// importScripts('/__/firebase/10.11.0/firebase-messaging-compat.js');
+// importScripts('/__/firebase/init.js');
+
+
+import {} from 'https://www.gstatic.com/firebasejs/10.11.0/firebase-app-compat.js';
+import {} from 'https://www.gstatic.com/firebasejs/10.11.0/firebase-messaging-compat.js';
+import {} from 'https://www.gstatic.com/firebasejs/init.js';
 
 
 const messaging = firebase.messaging();
