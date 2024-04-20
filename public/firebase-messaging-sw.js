e3fb2647d5e87bf5a9a4ad6d86bff2a2906d799e
AndyP <malicioussoap@gmail.com>
2024-04-20 14:56:46 -0500

    cull old code

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,10 +1,3 @@
-// Import and configure the Firebase SDK
-// These scripts are made available when the app is served or deployed on Firebase Hosting
-// If you do not serve/host your project using Firebase Hosting see https://firebase.google.com/docs/web/setup
-// importScripts('/__/firebase/10.11.0/firebase-app-compat.js');
-// importScripts('/__/firebase/10.11.0/firebase-messaging-compat.js');
-// importScripts('/__/firebase/init.js');
-
 
 importScripts('https://www.gstatic.com/firebasejs/10.11.0/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/10.11.0/firebase-messaging-compat.js');
