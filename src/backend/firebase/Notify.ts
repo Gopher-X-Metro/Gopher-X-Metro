f8eada0d62d767ed93ee15ec9937e7a03d5a9225
AndyP <malicioussoap@gmail.com>
2024-04-22 12:04:53 -0500

    removed debugging statements

--- src/backend/firebase/Notify.ts
+++ src/backend/firebase/Notify.ts
@@ -25,6 +25,9 @@ const supabase = createClient(
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRzbW9vd3FmbGtjZ3Nkcmx4aWNjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTIyNzU2NzYsImV4cCI6MjAyNzg1MTY3Nn0.Mbt6K4ZDGyzKiSuItnI68Yj47fCg6u-MIqu1sHcXrls",
 );
 
+
+let notificationPermission = false;
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const messaging = getMessaging(app);
@@ -33,19 +36,35 @@ onMessage(messaging, (payload) => {
   console.log("Message received via Notify TS. ", payload);
 });
 
+/**
+ * Requests permission for displaying notifications.
+ * @returns {Promise<boolean>} A promise that resolves to true if permission is granted, false otherwise.
+ */
 export async function requestPermission() {
   console.log("Requesting permission...");
   Notification.requestPermission().then((permission) => {
     if (permission === "granted") {
       console.log("Notification permission granted.");
+      notificationPermission = true;
       return true;
     }
   });
+  notificationPermission = false;
   return false;
 }
 
+/**
+ * Retrieves the FCM token for push notifications.
+ * @returns {Promise<void>} A promise that resolves with the FCM token.
+ */
 export async function getFCMToken() {
-  // Get registration token. Initially this makes a network call, once retrieved
+  if (notificationPermission === false) {
+    console.log("Notification permissions not properly allowed, cannot subscribe user to notifications")
+    return; 
+  }
+
+
+  // Initially this makes a network call, once retrieved
   // subsequent calls to getToken will return from cache.
   let token; 
   try {
@@ -55,15 +74,17 @@ export async function getFCMToken() {
   } catch (err) {
     console.log("found error where there should not be: " + err);
     console.log("this error initiated from Notify.ts, if you spot this error, the service worker may not have loaded expectedly")
+
   }
 
   console.log(token);
 }
 
-
+/**
+ * Registers a service worker when the page loads.
+ */
 export function registerServiceWorkerOnLoad() {
   if (navigator.serviceWorker) {
-    console.log("registering new service worker")
     // Register the SW
     navigator.serviceWorker.register('/firebase-messaging-sw.js', {scope: '/firebase-cloud-messaging-push-scope'}).then(function(registration){
     }).catch(console.log);
