5c6ae50b84a8ae4d9764913efe92eeec615fc572
Kennedy Johnson <92955299+KennedyJohnson@users.noreply.github.com>
2024-05-29 19:17:48 -0500

    working on getting stops showing up

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
-import Pages from './frontend/Pages/Pages.tsx';
+import Pages from './frontend/pages/Pages.tsx';
 import NavBar from './frontend/NavBar/NavBar.tsx';
 
 import "./styles.css"
