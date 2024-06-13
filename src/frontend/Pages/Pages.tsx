2b6d2a75b1e9af13899a434787d27f82d7d345a3
CubeToPlay <hwvxyeej@gmail.com>
2024-06-13 12:36:54 -0500

    Testing Schedules

--- src/frontend/Pages/Pages.tsx
+++ src/frontend/Pages/Pages.tsx
@@ -3,12 +3,14 @@ import { Route, Routes } from "react-router-dom";
 
 import Map from '../Map/Map.tsx';
 import About from './About/About.tsx';
+import Schedule from './Schedule/Schedule.tsx';
 
 function Pages() {
     return (
         <Routes>
             <Route path="/" element={<Map />} />
             <Route path="/about" element={<About />} />
+            <Route path="/schedule" element={<Schedule />} />
         </Routes>
     )
 }
