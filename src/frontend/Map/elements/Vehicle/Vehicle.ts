92ebeaacaa77b90840da7edd3990aa8b2e1ef175
Riley Thompson <130416939+riley-thomps@users.noreply.github.com>
2024-06-17 19:11:14 -0500

    bus direction implemented (will style better later)

--- src/frontend/Map/elements/Vehicle/Vehicle.ts
+++ src/frontend/Map/elements/Vehicle/Vehicle.ts
@@ -17,21 +17,45 @@ class Vehicle extends Element {
     constructor (vehicleId: string, color : string, map: google.maps.Map, images: string[2]) {
         super(vehicleId, color, map);
 
-        const content = document.createElement("div");
+        const contents = document.createElement("div");
+        contents.style.position = "relative";
 
+        // Create bus container
+        const busContainer = document.createElement("div");
+        busContainer.style.position = "absolute";
+        busContainer.style.left = "50%";
+        busContainer.style.top = "50%";
+        busContainer.style.transform = "translate(-50%, -50%)";
+
+        // Create arrow container
+        const arrowContainer = document.createElement("div");
+        arrowContainer.style.position = "absolute";
+        arrowContainer.style.left = "50%";
+        arrowContainer.style.top = "50%";
+        arrowContainer.style.transform = "translate(-50%, -50%)";
+        arrowContainer.style.top = "-10px";
+
+        // Create bus image
         const busImage = document.createElement("img")
         busImage.src = images[0];
-        busImage.width = 30;
-        content.appendChild(busImage);
+        busImage.width = 25;
+        busContainer.appendChild(busImage);
 
+        // Create arrow image
         const arrowImage = document.createElement("img")
         arrowImage.src = images[1];
-        arrowImage.width = 10;
-        content.appendChild(arrowImage);
+        arrowImage.width = 40;
+        arrowContainer.appendChild(arrowImage);
+
+        // Store reference to arrow image
+        this.arrowImg = arrowImage;
+
+        contents.appendChild(busContainer);
+        contents.appendChild(arrowContainer);
 
         this.marker = new window.google.maps.marker.AdvancedMarkerElement({
             map: map,
-            content: busImage,
+            content: contents,
         })
 
         this.infoWindow = new VehicleInfoWindow(vehicleId, map);
@@ -43,6 +67,7 @@ class Vehicle extends Element {
                 this.infoWindow.open();
         })
     }
+
     /**
      * Gets the length in ms of the time between when position was updated and now
      */
@@ -50,30 +75,36 @@ class Vehicle extends Element {
         if (this.timestamp)
             return (Date.now()/1000) - this.timestamp; 
     }
+
     /**
      * Gets the stopTimeUpdates array for the vehicle
      * @deprecated
      */
     public getStopTimeUpdates() : transit_realtime.TripUpdate.IStopTimeUpdate[] | undefined | null {return this.stopTimeUpdates; }
+
     /**
      * Get the trip ID
      */
     public getTripId() : string | undefined { return this.tripId; }
+
     /**
      * Get the marker object of this vehicle on the map
      */
     public getMarker() : google.maps.marker.AdvancedMarkerElement { return this.marker; }
+
     /**
      * Sets the stopTimeUpdates array for the vehicle 
      * @param stopTimeUpdates stopTimeUpdates array
      * @deprecated
      */
     public setStopTimeUpdates(stopTimeUpdates : transit_realtime.TripUpdate.IStopTimeUpdate[] | undefined | null) : void {this.stopTimeUpdates = stopTimeUpdates}
+
     /**
      * Sets the trip ID
      * @param tripId trip ID
      */
     public setTripId(tripId : string) : void { this.tripId = tripId; }
+
     /**
      * Sets the position of the vehicle on the map
      * @param position position of the vehicle
@@ -86,17 +117,23 @@ class Vehicle extends Element {
             this.timestamp = timestamp;
         }
     }
+
     /**
      * Gets the direction the bus is heading
      */
     public getBusBearing(): number | undefined { return this.bearing; }
+
     /**
      * Sets the direction the bus is heading
      * @param bearing the orientation of the bus
      */
     public setBusBearing(bearing: number): void {
         this.bearing = bearing;
+        if (this.arrowImg) {
+            this.arrowImg.style.transform = `rotate(${bearing}deg)`;
+        }
     }
+
     /**
      * Sets if the vehicle is visible
      * @param visible the visibility of the vehicle
@@ -111,6 +148,7 @@ class Vehicle extends Element {
     private marker: google.maps.marker.AdvancedMarkerElement;
     private stopTimeUpdates: transit_realtime.TripUpdate.IStopTimeUpdate[] | undefined | null;
     private bearing: number | undefined;
+    private arrowImg: HTMLImageElement | null = null;
     private infoWindow: VehicleInfoWindow;
 }
 
