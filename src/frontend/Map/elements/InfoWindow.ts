9465f2c739b87f5b92c6ca645ea846476f9bd0fc
CubeToPlay <hwvxyeej@gmail.com>
2024-06-18 17:12:16 -0500

    Slight Structure Change To InfoWindow

--- src/frontend/Map/elements/InfoWindow.ts
+++ src/frontend/Map/elements/InfoWindow.ts
@@ -3,12 +3,10 @@ export default class InfoWindow {
 
     /**
      * InfoWindow Constructor
-     * @param routeId   route ID the InfoWindow belongs to
      * @param location  location of the InfoWindow
      * @param map       map the InfoWindow displays on
      */
-    constructor(routeId: string, location: google.maps.LatLng | undefined, map: google.maps.Map) {
-        this.routeId = routeId;
+    constructor(location: google.maps.LatLng | undefined, map: google.maps.Map) {
         this.location = location;
         this.map = map;
         
@@ -61,7 +59,6 @@ export default class InfoWindow {
 
     /* Private */
 
-    protected routeId: string;
     protected location: google.maps.LatLng | undefined;
     protected window: google.maps.InfoWindow;
     protected map: google.maps.Map;
--- src/frontend/Map/elements/Stop/StopInfoWindow.ts
+++ src/frontend/Map/elements/Stop/StopInfoWindow.ts
@@ -1,14 +1,14 @@
-import InfoWindow from "../InfoWindow";
+import InfoWindowElement from "../InfoWindowElement";
 
-class StopInfoWindow extends InfoWindow {
+class StopInfoWindow extends InfoWindowElement {
     /**
      * Constructor for StopInfoWindow
      * @param routeId   ID of the route
      * @param location  Location of the infoWindow
      * @param map       Map the infowWindow is put on
      */
-    constructor(routeId: string, location: google.maps.LatLng, map: google.maps.Map) {
-        super(routeId, location, map);
+    constructor(element: google.maps.MVCObject | google.maps.marker.AdvancedMarkerElement, location: google.maps.LatLng, map: google.maps.Map) {
+        super(element, location, map);
     }
 }
 
