56b1b5a759d13ebbfec922b01e769200a8fe72a7
Andy Phu <51890861+andyjphu@users.noreply.github.com>
2024-07-10 18:18:26 -0500

    complete deprecation

--- src/frontend/NavBar/components/TitleBar.tsx
+++ src/frontend/NavBar/components/TitleBar.tsx
@@ -3,7 +3,6 @@ import { Heading, Box } from "@chakra-ui/react"
 
 
 export default function TitleBar(){
-    //TODO: find out if we wanted to keep the older title bar
     const xStyle = {
         'background': "linear-gradient(to right, #FFCC33 50%, #0053A0 50%)",
         'backgroundClip': 'text',
@@ -14,9 +13,9 @@ export default function TitleBar(){
     return(
         <div>
             <Box pos='absolute' top='1.5' left='200'>
-                <Heading display='inline-block' color='#FFCC33' margin='1'>Gopher Buses </Heading>
+                <Heading display='inline-block' color='#FFCC33' margin='1'>Gopher </Heading>
                 <Heading display='inline-block' margin='1' style={xStyle}> X </Heading>
-                <Heading display='inline-block' color ='#0053A0' margin='1'> Metro Buses </Heading>
+                <Heading display='inline-block' color ='#0053A0' margin='1'> Metro </Heading>
             </Box>
         </div>
         
