7a9b4ce10aed18be64a8241d8927ec89bd615358
Andy Phu <51890861+andyjphu@users.noreply.github.com>
2024-07-09 23:03:28 -0500

    fixed <h2> cannot be child of <h2> error

--- src/frontend/NavBar/components/TitleBar.tsx
+++ src/frontend/NavBar/components/TitleBar.tsx
@@ -1,23 +1,23 @@
 import React from "react"
-import { Heading } from "@chakra-ui/react"
+import { Heading, Box } from "@chakra-ui/react"
 
 
 export default function TitleBar(){
 
     const xStyle = {
         'background': "linear-gradient(to right, #FFCC33 50%, #0053A0 50%)",
-        'background-clip': 'text',
+        'backgroundClip': 'text',
         'WebkitBackgroundClip': 'text',
         'WebkitTextFillColor': 'transparent',
     }
 
     return(
         <div>
-            <Heading as='h1' size='2xl' pos='absolute' top='1.5' left='200'>
+            <Box pos='absolute' top='1.5' left='200'>
                 <Heading display='inline-block' color='#FFCC33' margin='1'>Gopher Buses </Heading>
                 <Heading display='inline-block' margin='1' style={xStyle}> X </Heading>
                 <Heading display='inline-block' color ='#0053A0' margin='1'> Metro Buses </Heading>
-            </Heading>
+            </Box>
         </div>
         
     )
