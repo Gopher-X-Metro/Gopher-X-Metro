11ea4d825c46e7de11f24ef62aa98405069848da
AndyP <malicioussoap@gmail.com>
2024-07-10 19:46:10 -0500

    styling fix for hamburger

--- src/frontend/NavBar/NavBar.tsx
+++ src/frontend/NavBar/NavBar.tsx
@@ -15,34 +15,35 @@ export default function NavBar() {
 
     return (
         <div id="title-bar">
-            <HStack padding="3px">
+            <HStack padding="24px" gap="30px">
 
-            <SideBar />
+                <SideBar />
                 <Box>
                     <Link to="/" >
                         <TitleBar />
                     </Link>
                 </Box>
-                <div id="main">
+                <div className="max-md:hidden">
+                    
                     <Link to="/schedules" >
-                        <Button pos="absolute" top="5" right="5" colorScheme='yellow'>
+                        <Button colorScheme='yellow'>
                             Schedules
                         </Button>
                     </Link>
                     <a href='https://pts.umn.edu/sites/pts.umn.edu/files/2020-07/bus_outline_map_printable.jpg' target="_blank" rel="noreferrer">
-                        <Button pos="absolute" top="5" right="150" colorScheme='yellow'>
+                        <Button colorScheme='yellow'>
                             Campus Bus Map
                         </Button>
                     </a>
 
                     <a href='https://umn.rider.peaktransit.com' target="_blank" rel="noreferrer">
-                        <Button pos="absolute" top="5" right="335" colorScheme='yellow'>
+                        <Button colorScheme='yellow'>
                             GopherTrip Map
                         </Button>
                     </a>
 
                     <Link to="/about" >
-                        <Button pos="absolute" top="5" right="520" colorScheme='yellow'>
+                        <Button colorScheme='yellow'>
                             About
                         </Button>
                     </Link>
