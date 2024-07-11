2f67ec8694c0e7a148115a36f4315abe09325719
Adam Yaj <86120028+CubeToPlay@users.noreply.github.com>
2024-07-11 11:28:24 -0500

    The route infowindow now activates routes in the sidebar

--- src/frontend/NavBar/components/RouteButton.tsx
+++ src/frontend/NavBar/components/RouteButton.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 
 import URL from 'src/backend/URL.ts';
 import Routes from 'src/frontend/Map/Routes.ts';
@@ -10,32 +10,38 @@ import Routes from 'src/frontend/Map/Routes.ts';
  * @returns 
  */
 function RouteButton({ routeId, text }) {
-  const [_, setForceUpdate] = useState(0);
-  const isActive = Array.from(URL.getRoutes()).includes(routeId); // used to check if route button is active
+  useEffect(() => {
+    URL.addListener(() => {
+      setActive(URL.getRoutes().has(routeId)); // updates color of button click immediately
+    })
+    
+    setActive(URL.getRoutes().has(routeId));
+  }, [])
 
+  const [isActive, setActive] = useState(false);
+  
   const removeInfoWindows = () => {
     // Get all markers associated with the routeId
     const stops = Routes.getRoute(routeId)?.getStops();
     // Iterate over markers and close their info windows
-    if (stops)
-        stops.forEach(stop => { stop.closeInfoWindow(); });
+    stops?.forEach(stop => { stop.closeInfoWindow(); });
   };
-  
-  return (
-      <button className={`route-btn ${isActive ? 'active' : ''} route-${routeId}`} onClick={() => {
-          // selects specific route depending on button pressed
-          if (!URL.getRoutes().has(routeId))
-              URL.addRoute(routeId);
-          else
-              URL.removeRoute(routeId);
 
-          // Remove info windows associated with the routeId
-          removeInfoWindows();
-          setForceUpdate(Math.random()); // updates color of button click immediately
-      }}>
-          {text}
-      </button>
-  )
+  const buttonElement = React.createElement("button", {
+    className: `route-btn ${isActive ? 'active' : undefined} route-${routeId}`,
+    onClick: () => {
+      // selects specific route depending on button pressed
+      if (!isActive)
+        URL.addRoute(routeId);
+      else
+        URL.removeRoute(routeId);
+
+      // Remove info windows associated with the routeId
+      removeInfoWindows();
+    }
+  }, text);
+
+  return buttonElement;
 }
 
 export default RouteButton;
