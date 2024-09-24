47568dbf05755cc4fe2de58c6dccf569e90bc2cc
CubeToPlay <hwvxyeej@gmail.com>
2024-09-23 20:07:13 -0500

    Removed redundant imports, removed about page dropdown

--- src/frontend/Pages/About/About.tsx
+++ src/frontend/Pages/About/About.tsx
@@ -9,22 +9,6 @@ function About({ hidden, setPage }) {
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
                 <div className="container">
                     <button className="navbar-brand" onClick={() => setPage("map")}>Back to Map</button>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
-                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-                            <li className="nav-item"><a className="nav-link active" aria-current="page" href="https://pts.umn.edu/Transit/Transit-Services/Campus-Buses">Campus Bus Map</a></li>
-                            <li className="nav-item"><a className="nav-link" href="https://umn.rider.peaktransit.com/">GopherTrip Map</a></li>
-                            <li className="nav-item dropdown">
-                                <a className="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Maps and Schedules</a>
-                                <ul className="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                    <li><a className="dropdown-item" href="https://umn.rider.peaktransit.com/">GopherTrip Map</a></li>
-                                    <li><a className="dropdown-item" href="https://pts.umn.edu/Transit/Transit-Services/Campus-Buses">Campus Bus Map</a></li>
-                                    <li><hr className="dropdown-divider" /></li>
-                                    {/* <li><a className="dropdown-item" href="#">Something else here</a></li> */}
-                                </ul>
-                            </li>
-                        </ul>
-                    </div>
                 </div>
             </nav>
             {/* <!-- Page content--> */}
