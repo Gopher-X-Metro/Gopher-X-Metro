c143ae4440e69f2848d6572d0c878b4b548e1f9a
Adam Yaj <86120028+CubeToPlay@users.noreply.github.com>
2024-10-15 17:07:21 -0500

    Fixed Dropdown

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import { BrowserRouter } from 'react-router-dom';
 import { APIProvider } from '@vis.gl/react-google-maps';
@@ -11,6 +11,13 @@ import "./styles.css";
 export default function App() {
   // Check if screen width is 1024px or less (common mobile size and before buttons are cutoff by title)
   const [isMobile] = useMediaQuery("(max-width: 1024px)");
+
+  useEffect(() => {
+    if (isMobile)
+      document.body.className = document.body.className + " mobile";
+    else
+      document.body.className = document.body.className.replace(" mobile", "");
+  }, [isMobile])
 
   return (
     <APIProvider apiKey={process.env.REACT_APP_API_KEY} libraries={["places", "marker"]}>
