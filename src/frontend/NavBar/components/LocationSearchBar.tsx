0470886e2157674ea590b581a5a5a71668e2eb6c
Adam Yaj <hwvxyeej@gmail.com>
2024-10-15 17:09:46 -0500

    Fixed for mobile

--- src/frontend/NavBar/components/LocationSearchBar.tsx
+++ src/frontend/NavBar/components/LocationSearchBar.tsx
@@ -1,42 +1,29 @@
-import { ControlPosition, MapControl, useMap, useMapsLibrary } from "@vis.gl/react-google-maps";
-import React, { useEffect, useRef, useState } from "react";
+import { ControlPosition, MapControl, useMap } from "@vis.gl/react-google-maps";
+import React, { useEffect, useRef } from "react";
 import Plan from "src/backend/Plan";
 import Routes from "src/frontend/Pages/Map/components/Routes";
 import Search from "src/frontend/Pages/Map/elements/Search";
-import { centerMap } from "src/frontend/Pages/Map/Map";
 
 const searches = new Map<string, Search>();
 
 export default function LocationSearchBar( { isMobile } ) {
-    const [placeAutocomplete, setPlaceAutocomplete] = useState<google.maps.places.Autocomplete | null>(null);
-    const inputRef = useRef<HTMLInputElement>(null);
-    const places = useMapsLibrary('places');
-    
+    const input = useRef<HTMLInputElement>(null);
     const map = useMap("map");
 
     useEffect(() => {
-        if (!places || !inputRef.current) return;
-
-        const options = { 
-            fields: ['place_id', 'geometry', 'name', 'formatted_address'],
-            componentRestrictions: { country: "us" } 
-        };
-
-        setPlaceAutocomplete(new places.Autocomplete(inputRef.current, options));
-    }, [places]);
-
-    useEffect(() => {
-        if (!placeAutocomplete || !map) return;
-
-        placeAutocomplete.bindTo("bounds", map);
-
-        placeAutocomplete.addListener('place_changed', () => onPlaceSelect(map, placeAutocomplete.getPlace()));
-    }, [onPlaceSelect, placeAutocomplete]);
+        if (input.current && map) {
+            const autocomplete = new google.maps.places.Autocomplete(input.current, { fields: ["place_id", "geometry", "name", "formatted_address"] });
+            const geocoder = new google.maps.Geocoder();
+    
+            autocomplete.bindTo("bounds", map);
+            autocomplete.addListener("place_changed", () => onPlaceChange(map, autocomplete, geocoder));
+        }
+    }, [input, map])
 
     return (
         <>
             <MapControl position={isMobile ? ControlPosition.BOTTOM_CENTER : ControlPosition.TOP_CENTER}>
-                <input id="location-search-bar" className={"location-search-bar" + (isMobile ? " mobile" : "")} type="text" ref={inputRef}/>
+                <input id="location-search-bar" className={"location-search-bar" + (isMobile ? " mobile" : "")} type="text" ref={input}/>
             </MapControl>
         </>
     )
@@ -45,32 +32,35 @@ export default function LocationSearchBar( { isMobile } ) {
 /**
      * Sets the new location of the marker, and focuses on the spot
      */
-function onPlaceSelect(map : google.maps.Map, place : google.maps.places.PlaceResult) : void {
-    console.log(place);
+function onPlaceChange(map : google.maps.Map, autocomplete : google.maps.places.Autocomplete, geocoder : google.maps.Geocoder) : void {
+    const place = autocomplete.getPlace();
 
-    if (!place || !place.place_id) return;
-
-    const location = place.geometry?.location;
-
-    if (!location) return;
-
-    centerMap(map, location, 15);
+    if (!place.place_id) return;
 
     if (searches.has(place.place_id)) {
         searches.get(place.place_id)?.setVisible(true);
     } else {
-        searches.set(place.place_id as string, new Search(place.place_id as string, place.name, location, map));
+        geocoder
+        .geocode({ placeId: place.place_id })
+        .then(async ({ results }) => {
+            const location = results[0].geometry.location;
 
-        Plan.serviceNearby(location.lat(), location.lng(), null, 0, 0.3).then(async nearest => {
-            if (nearest.version !== 0) {
-                for (const stop of nearest.atstop) {
-                    Routes.loadStop(stop.stopid, "").then(s => {
-                        if (s) searches.get(place.place_id as string)?.addElement(s);
-                        s?.addElement(searches.get(place.place_id as string) as Search);
-                        s?.updateVisibility();
-                    });
+            searches.set(place.place_id as string, new Search(place.place_id as string, place.name, location, map));
+
+            map.setZoom(15);
+            map.setCenter(location);
+
+            Plan.serviceNearby(location.lat(), location.lng(), null, 0, 0.3).then(async nearest => {
+                if (nearest.version !== 0) {
+                    for (const stop of nearest.atstop) {
+                        Routes.loadStop(stop.stopid, "").then(s => {
+                            if (s) searches.get(place.place_id as string)?.addElement(s);
+                            s?.addElement(searches.get(place.place_id as string) as Search);
+                            s?.updateVisibility();
+                        });
+                    }
                 }
-            }
-        })
+            })
+        }).catch((error) => window.alert("Geocoder failed due to: " + error));
     }
 }
