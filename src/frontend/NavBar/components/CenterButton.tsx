6f19ae4cb9695be70446471e55963ab82f75c621
Adam Yaj <hwvxyeej@gmail.com>
2024-10-15 22:58:58 -0500

    Made centering more obvious

--- src/frontend/NavBar/components/CenterButton.tsx
+++ src/frontend/NavBar/components/CenterButton.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { MdFilterCenterFocus } from "react-icons/md";
+import { IoMdLocate } from "react-icons/io";
 import { ControlPosition, MapControl, useMap } from "@vis.gl/react-google-maps";
 import { centerMap } from "src/frontend/Pages/Map/Map";
 
@@ -7,19 +7,42 @@ export default function CenterButton() {
     const [centered, setCentered] = useState(true);
     const map = useMap("map");
 
+    const onClick = () => {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition((position) => {
+                if (position.coords.accuracy <= 1000) {
+                    centerMap(map, {lat: position.coords.latitude, lng: position.coords.longitude}); 
+                    setCentered(true);
+                } else
+                    console.warn("Your location is too inaccurate to center.");
+            }, () => console.warn("Unable to retreive your location."));
+        }
+
+        if (!centered) {
+            centerMap(map); 
+            setCentered(true);
+        }
+    }
 
     useEffect(() => {
         if (map) {
             map.addListener("center_changed", () => setCentered(false));
             map.addListener("zoom_changed", () => setCentered(false));
+            map.addListener("drag", () => setCentered(false));
         }
     }, [map])
 
     return (
         <>
             <MapControl position={ControlPosition.RIGHT_BOTTOM}>
-                <button id="center-button" className={"center-button" + (centered ? " centered" : "")} onClick={() => {centerMap(map); setCentered(true)}}>
-                    <MdFilterCenterFocus className="icon"/>
+                <button id="center-button" 
+                        draggable="false"
+                        aria-label="Center"
+                        title="Center"
+                        type="button"
+                        className={"center-button" + (centered ? " centered" : "")} 
+                        onClick={() => onClick()}>
+                    <IoMdLocate className="icon"/>
                 </button>
             </MapControl>
         </>
