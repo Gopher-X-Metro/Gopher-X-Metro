bb9261dcdb929614a8897ecf49cb20ec3ff3ef36
Riley Thompson <130416939+riley-thomps@users.noreply.github.com>
2024-10-21 15:49:17 -0500

    styled CenterButton

--- src/frontend/NavBar/components/CenterButton.tsx
+++ src/frontend/NavBar/components/CenterButton.tsx
@@ -3,10 +3,23 @@ import { IoMdLocate } from "react-icons/io";
 import { ControlPosition, MapControl, useMap } from "@vis.gl/react-google-maps";
 import { centerMap } from "src/frontend/Pages/Map/Map";
 
+
+/**
+ * CenterButton Component
+ * 
+ * Button to center map to user's current location or default center.
+ * 
+ * Button listens for map events such as 'center_changed', 'zoom_changed', and 'drag'
+ * to update 'centered' state. This state determines whether the map is centered on 
+ * the user's current location or not
+ * 
+ * @returns rendered CenterButton component
+ */
 export default function CenterButton() {
     const [centered, setCentered] = useState(true);
     const map = useMap("map");
 
+    // Handles click event to center map
     const onClick = () => {
         if (navigator.geolocation) {
             navigator.geolocation.getCurrentPosition((position) => {
@@ -19,19 +32,21 @@ export default function CenterButton() {
             }, () => { console.warn("Unable to retreive your location.") });
         }
 
+        // Re-center to default map position if not already centered
         if (!centered) {
             centerMap(map); 
             setCentered(true);
         }
     }
 
+    // Adds listeners to map for certain events to set the 'centered' state to false
     useEffect(() => {
         if (map) {
             map.addListener("center_changed", () => setCentered(false));
             map.addListener("zoom_changed", () => setCentered(false));
             map.addListener("drag", () => setCentered(false));
         }
-    }, [map])
+    }, [map]);
 
     return (
         <>
@@ -47,5 +62,5 @@ export default function CenterButton() {
                 </button>
             </MapControl>
         </>
-    )
+    );
 }
