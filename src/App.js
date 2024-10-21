f19677f535d882794315d2174e2a83281f814d4d
unknown <rileythompson282@gmail.com>
2024-10-21 13:53:40 -0500

    styled index and App

--- src/App.js
+++ src/App.js
@@ -1,28 +1,34 @@
 import React, { useEffect } from 'react';
-
 import { BrowserRouter } from 'react-router-dom';
 import { APIProvider } from '@vis.gl/react-google-maps';
 import { useMediaQuery } from '@chakra-ui/react';
 
-import Pages from './frontend/Pages/Pages.tsx';
+import Pages from 'src/frontend/Pages/Pages.tsx';
 
-import "./styles.css";
+import "src/styles.css";
 
+/**
+ * Main component of application responsible for handling layout and API context
+ * Detects screen size and adjusts the body's class based on mobile or desktop layout
+ * 
+ * @returns rendered application component.
+ */
 export default function App() {
   // Check if screen width is 1024px or less (common mobile size and before buttons are cutoff by title)
   const [isMobile] = useMediaQuery("(max-width: 1024px)");
 
   useEffect(() => {
-    if (isMobile)
-      document.body.className = document.body.className + " mobile";
-    else
+    if (isMobile) {
+      document.body.className += " mobile";
+    } else {
       document.body.className = document.body.className.replace(" mobile", "");
-  }, [isMobile])
+    }
+  }, [isMobile]);
 
   return (
     <APIProvider apiKey={process.env.REACT_APP_API_KEY} libraries={["places", "marker"]}>
       <BrowserRouter>
-          <Pages isMobile={isMobile}/>
+          <Pages isMobile={isMobile} />
       </BrowserRouter>
     </APIProvider>
   );
