569eba3c3e551cac55cd589714c4758812d6a797
Riley Thompson <130416939+riley-thomps@users.noreply.github.com>
2024-10-23 09:48:21 -0500

    styled LoadingScreen

--- src/frontend/Pages/Map/components/LoadingScreen.tsx
+++ src/frontend/Pages/Map/components/LoadingScreen.tsx
@@ -1,22 +1,16 @@
 import React from "react";
 
 /**
- * The loading screen component
+ * Loading screen component
  * @param hidden if the screen should be hidden
+ * @returns rendered loading screen component
  */
 export default function LoadingScreen({ hidden }) {
   return (
     <>
       {!hidden && (
-
-        <div
-          id="loading-screen"
-          className="fixed w-[100vw] h-[100vh] top-0 left-0 z-50 flex justify-center items-center"
-          style={{
-            backgroundColor: 'rgba(255, 255, 255, 0.9)',
-          }}
-        >
-          <div className="fixed w-[100vw] h-[50vw]" >
+        <div id="loading-screen" className="fixed w-[100vw] h-[100vh] top-0 left-0 z-50 flex justify-center items-center" style={{backgroundColor: "rgba(255, 255, 255, 0.9)"}}>
+          <div className="fixed w-[100vw] h-[50vw]">
             <img src="/LoadingScreenAssets/BusStatic.svg" className="absolute left-[25%] top-[33%] w-[50%] h-auto" alt="A bus with animated rolling wheels" />
             <img src="/LoadingScreenAssets/Wheel.svg" className="rotate-wheel absolute left-[39.35%] top-[54%] w-[5.3%] h-auto" alt="A rolling wheel" />
             <img src="/LoadingScreenAssets/Wheel.svg" className="rotate-wheel absolute left-[66%] top-[53.5%] transform w-[5.3%] h-auto" alt="A rolling wheel" />
@@ -24,8 +18,7 @@ export default function LoadingScreen({ hidden }) {
             <h1 className="fixed top-[65%] bottom-[50%] left-[50%] transform -translate-x-1/2 -translate-y-1/2 text-[5vw]	">Loading...</h1>
           </div>
         </div>
-
       )}
     </>
   );
-};
+}
