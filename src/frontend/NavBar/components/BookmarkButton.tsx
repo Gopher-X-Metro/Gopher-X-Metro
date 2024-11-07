fe42072c8a72cbe0a1dca2089563ca44f7276852
Babacar <Babadias0215@gmail.com>
2024-11-06 23:47:04 -0600

    update

--- src/frontend/NavBar/components/BookmarkButton.tsx
+++ src/frontend/NavBar/components/BookmarkButton.tsx
@@ -1,69 +1,41 @@
 import URL from 'src/backend/URL.ts';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { BsBookmarkStar } from 'react-icons/bs';
 import { ControlPosition, MapControl } from '@vis.gl/react-google-maps';
 import ReactDOM from 'react-dom/client';
 import RouteButton from './RouteButton.tsx';
 import Schedule from 'src/backend/Schedule.ts';
+import { ring } from '@chakra-ui/react';
 
 
-    
 
 export function BookmarkButton() {
     const [, forceReload] = useState(1);
     const [bookmarked, setBookmarked] = useState(false); 
-    const [routes, setRoutes] = useState(new Map<string,string>());
+    const [routes, setRoutes] = useState(new Set<string>());
+    const rootRef = useRef(null);
 
 
 
-    useEffect(() => {
-      
-        /**
-         * Convert a string to a properly cased title
-         * @param input input string
-         */
-        const toTitleCase = (input : string) => { 
-            return input.replace( 
-                /\w\S*/g, 
-                text => text.charAt(0).toUpperCase() + text.substring(1).toLowerCase() 
-            ); 
-        }
+  useEffect(() => setRoutes(URL.getRoutes()), [])
 
-        /**
-         * Updates the displayed routes on the sidebar
-         */ 
-        const change = async () => {
-            for (const routeId of URL.getRoutes()) {
-                if (!routes.has(routeId)) {
-                    const info = await Schedule.getRoute(routeId);
-                    const name = info ? info.route_label : routeId;
-                    routes.set(routeId, toTitleCase(name));
-                }
-            }
-
-            setRoutes(routes);
-            forceReload(Math.random());
-        }
-
-        URL.addListener(() => change());
-
-        change();
-    }, [])
+    URL.addListener(() => {
+        forceReload(Math.random());
+        setRoutes(URL.getRoutes());
+    });
 
     
 
     const onClick = () => {
         // TODO: setBookmarked to flip the bookmark of a route
-        // Temp
+        setBookmarked(true);
 
-        // routes.set("123", "Route 123");
-
+        const root = ReactDOM.createRoot(document.getElementById('Favorite-tab'));
+        console.log(routes.size, "size");
      
-         const root = ReactDOM.createRoot(document.getElementById('Favorite-tab'));
-
-         const element =   <div>
+         const element = <div>
                  {Array.from(routes.keys()).map((routeId) => 
-                <RouteButton key={routeId} routeId={routeId} text={routeId}/>
+                <RouteButton key={routeId} routeId={routeId} text={"Route " + routeId}/>
                 
                 )}
             </div>
@@ -71,14 +43,15 @@ export function BookmarkButton() {
           
         
         console.log(routes);
-        setBookmarked(true);
+        
       
     }
 
     return (
         <>
             <MapControl position={ControlPosition.RIGHT_BOTTOM}>
-                <button hidden={routes.size !== 1}
+              
+                <button hidden={URL.getRoutes().size !== 1}
                         id="bookmark-button" 
                         draggable="false"
                         aria-label="Center"
