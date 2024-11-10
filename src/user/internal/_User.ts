99c957f5a763cbfaf9abc8b6c779d0bf966215b1
Adam Yaj <86120028+CubeToPlay@users.noreply.github.com>
2024-11-10 16:25:11 -0600

    Added Comments

--- src/user/internal/_User.ts
+++ src/user/internal/_User.ts
@@ -1,9 +1,15 @@
+/** The class that is used to define a User*/
 class _User {
+    /** The user's ID */
+    public readonly id: string;
+
+    /**
+     * Constructor for the _User class
+     * @param id ID of the User
+     */
     constructor(id: string) {
         this.id = id;
     }
-
-    public readonly id: string;
 }
 
 export default _User;
