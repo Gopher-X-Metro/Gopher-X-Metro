c70c194795bb858a4a5f8429d4fe1851bc5fecf5
CubeToPlay <86120028+CubeToPlay@users.noreply.github.com>
2024-11-10 16:00:11 -0600

    Update src/data/internal/_Route.ts
    Co-authored-by: Riley Thompson <130416939+riley-thomps@users.noreply.github.com>

--- src/data/internal/_Route.ts
+++ src/data/internal/_Route.ts
@@ -34,7 +34,7 @@ export default class _Route extends _DataAbstract {
 
         // Load Directions
         await Realtime.getDirections(this.id as string).then(response => {
-            for (const direction of response)
+            for (const direction of response) {
                 this.directions.set(direction.direction_id, Data.Direction.create(direction.direction_id, this.id as string));
         })
     }
