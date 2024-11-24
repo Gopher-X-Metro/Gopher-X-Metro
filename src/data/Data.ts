75d88a1c5c504d424379162a677cf858cd9f0a8d
Adam Yaj <hwvxyeej@gmail.com>
2024-11-24 17:57:22 -0600

    Added An Interface for data types in Data

--- src/data/Data.ts
+++ src/data/Data.ts
@@ -464,6 +464,69 @@ namespace Data {
             }
         }
     };
+
+    export namespace Interface {
+        export interface Agency {
+            agency_id: number;
+            agency_name: string;
+        }
+        export interface AlertMessage {
+            stop_closed: boolean;
+            alert_text: string;
+        }
+        export interface Depature {
+            actual: boolean;
+            trip_id: string;
+            stop_id: number;
+            departure_text: string;
+            departure_time: number;
+            description: string;
+            gate: string;
+            route_id: string;
+            route_short_name: string;
+            direction_id: number;
+            direction_text: string;
+            terminal: string;
+            schedule_relationship: string;
+        }
+        export interface Direction {
+            direction_id: number;
+            direction_name: string;
+        }
+        export interface Place {
+            place_code: string;
+            description: string;
+        }
+        export interface Route {
+            route_id: string;
+            agency_id: number;
+            route_label: string;
+        }
+        export interface Stop {
+            stop_id: number;
+            latitude: number;
+            longitude: number;
+            description: string;
+        }
+        export interface Vehicle {
+            trip_id: string;
+            direction_id: number;
+            direction: string;
+            location_time: number;
+            route_id: string;
+            terminal: string;
+            latitude: number;
+            longitude: number;
+            bearing: number;
+            odometer: number;
+            speed: number;
+        }
+        export interface PlaceDetail {
+            stops: Array<Stop>;
+            alerts: Array<AlertMessage>;
+            departures: Array<Departure>;
+        }
+    }
 }
 
 export default Data;
