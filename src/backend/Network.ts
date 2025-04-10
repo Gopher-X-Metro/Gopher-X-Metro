ea52a167a0e37537552a4d1d0392df0d9395a37f
Adam Yaj <hwvxyeej@gmail.com>
2025-04-09 22:45:33 -0500

    Add Peak to Network

--- src/backend/Network.ts
+++ src/backend/Network.ts
@@ -15,34 +15,25 @@ export class Network {
         return Network.#instance;
     }
 
-    public async getTrips(routeId : string) : Promise<ITrip | undefined> {
-        const value = TripsNetworkRetriever.instance.retrieve<ITrip>(routeId);
-        
-        if (!value) {
-            throw new RetreiveError("Network getTrip could not get a trip!");
-        }
-        
-        return value;
+    public async getMetroTrips(routeId : string) : Promise<Array<ITrip> | undefined> {
+        return TripsMetroNetworkRetriever.instance.retrieve<Array<ITrip>>(routeId);
     }
 
-    public async getShapes(shapeId : string) : Promise<IShape | undefined> {
-        const value = ShapesNetworkRetriever.instance.retrieve<IShape>(shapeId);
-        
-        if (!value) {
-            throw new RetreiveError("Network getShapes could not get a shape!");
-        }
-
-        return value;
+    public async getMetroShapes(shapeId : string) : Promise<Array<IShape> | undefined> {
+        return ShapesMetroNetworkRetriever.instance.retrieve<Array<IShape>>(shapeId);
     }
 
-    public async getCalendar(serviceId : string) : Promise<ICalendar | undefined> {
-        const value = CalendarNetworkRetriever.instance.retrieve<ICalendar>(serviceId);
-        
-        if (!value) {
-            throw new RetreiveError("Network getCalendar could not get a calendar!");
-        }
-     
-        return value;
+    public async getMetroCalendar(serviceId : string) : Promise<ICalendar | undefined> {
+        return CalendarMetroNetworkRetriever.instance.retrieve<ICalendar>(serviceId);
+    }
+
+
+    public async getPeakTrips(routeId : string) : Promise<Array<any> | undefined> {
+        return TripsPeakNetworkRetriever.instance.retrieve<Array<any>>(routeId);
+    }
+
+    public async getPeakShapes(shapeId : string) : Promise<Array<any> | undefined> {
+        return ShapesPeakNetworkRetriever.instance.retrieve<Array<any>>(shapeId);
     }
 }
 
@@ -56,50 +47,60 @@ class RetreiveError extends Error {
 }
 
 abstract class NetworkRetriever {
+    constructor(API_URL : string) { this.API_URL = API_URL; }
+
     abstract retrieve<Interface>(data : string) : Promise<Interface | undefined>;
 
-    /* API base URL */
-    public static readonly API_URL : string = process.env.REACT_APP_SUPABASE_FUNCTION_URL as string;
+    protected readonly API_URL : string;
 }
 
-class TripsNetworkRetriever extends NetworkRetriever {
-    static #instance: TripsNetworkRetriever;
+abstract class MetroNetworkRetriever extends NetworkRetriever {
+    constructor() { super(process.env.REACT_APP_SUPABASE_FUNCTION_URL as string); }
+}
+
+abstract class PeakNetworkRetriever extends NetworkRetriever {
+    constructor() { super("https://api.peaktransit.com/v5/index.php?app_id=_RIDER&key=c620b8fe5fdbd6107da8c8381f4345b4&controller="); }
+    protected readonly API_URL_END : string = "&action=list&agencyID=88";
+}
+
+class TripsMetroNetworkRetriever extends MetroNetworkRetriever {
+    static #instance: TripsMetroNetworkRetriever;
     
     private constructor() {super();}
 
-    public static get instance(): TripsNetworkRetriever {
-        if (!TripsNetworkRetriever.#instance) {
-            TripsNetworkRetriever.#instance = new TripsNetworkRetriever();
+    public static get instance(): TripsMetroNetworkRetriever {
+        if (!TripsMetroNetworkRetriever.#instance) {
+            TripsMetroNetworkRetriever.#instance = new TripsMetroNetworkRetriever();
         }
 
-        return TripsNetworkRetriever.#instance;
+        return TripsMetroNetworkRetriever.#instance;
     }
 
-    async retrieve<ITrip>(routeId : string): Promise<ITrip | undefined> {
-        return fetch(`${NetworkRetriever.API_URL}/api/get-trips?route_id=${routeId}`)
+    async retrieve<ITrips>(routeId : string): Promise<ITrips | undefined> {
+        return fetch(`${this.API_URL}/api/get-trips?route_id=${routeId}`)
         .then((response : Response) => 
             response.ok ? 
-            response.json().then((json : ITrip) => json) 
+            response.json().then((json : ITrips) => json) 
             : undefined
         );
     }
 }
 
-class ShapesNetworkRetriever extends NetworkRetriever {
-    static #instance: ShapesNetworkRetriever;
+class ShapesMetroNetworkRetriever extends MetroNetworkRetriever {
+    static #instance: ShapesMetroNetworkRetriever;
     
     private constructor() {super();}
 
-    public static get instance(): ShapesNetworkRetriever {
-        if (!ShapesNetworkRetriever.#instance) {
-            ShapesNetworkRetriever.#instance = new this();
+    public static get instance(): ShapesMetroNetworkRetriever {
+        if (!ShapesMetroNetworkRetriever.#instance) {
+            ShapesMetroNetworkRetriever.#instance = new this();
         }
 
-        return ShapesNetworkRetriever.#instance;
+        return ShapesMetroNetworkRetriever.#instance;
     }
 
     async retrieve<IShapes>(routeId : string): Promise<IShapes | undefined> {
-        return fetch(`${NetworkRetriever.API_URL}/api/get-shapes?shape_id=${routeId}`)
+        return fetch(`${this.API_URL}/api/get-shapes?shape_id=${routeId}`)
         .then((response : Response) => 
             response.ok ? 
             response.json().then((json : IShapes) => json) 
@@ -108,25 +109,81 @@ class ShapesNetworkRetriever extends NetworkRetriever {
     }
 }
 
-class CalendarNetworkRetriever extends NetworkRetriever {
-    static #instance: CalendarNetworkRetriever;
+class CalendarMetroNetworkRetriever extends MetroNetworkRetriever {
+    static #instance: CalendarMetroNetworkRetriever;
     
     private constructor() {super();}
 
-    public static get instance(): CalendarNetworkRetriever {
-        if (!CalendarNetworkRetriever.#instance) {
-            CalendarNetworkRetriever.#instance = new CalendarNetworkRetriever();
+    public static get instance(): CalendarMetroNetworkRetriever {
+        if (!CalendarMetroNetworkRetriever.#instance) {
+            CalendarMetroNetworkRetriever.#instance = new CalendarMetroNetworkRetriever();
         }
 
-        return CalendarNetworkRetriever.#instance;
+        return CalendarMetroNetworkRetriever.#instance;
     }
 
-    async retrieve<ITrip>(routeId : string): Promise<ITrip | undefined> {
-        return fetch(`${NetworkRetriever.API_URL}/api/get-calendar?service_id=${routeId}`)
+    async retrieve<ICalendar>(routeId : string): Promise<ICalendar | undefined> {
+        return fetch(`${this.API_URL}/api/get-calendar?service_id=${routeId}`)
         .then((response : Response) => 
             response.ok ? 
-            response.json().then((json : ITrip) => json) 
+            response.json().then((json : ICalendar) => json) 
             : undefined
         );
+    }
+}
+
+class TripsPeakNetworkRetriever extends PeakNetworkRetriever {
+    static #instance: TripsPeakNetworkRetriever;
+    static #trips: Array<any> | undefined;
+    
+    private constructor() {super();}
+
+    public static get instance(): TripsPeakNetworkRetriever {
+        if (!TripsPeakNetworkRetriever.#instance) {
+            TripsPeakNetworkRetriever.#instance = new TripsPeakNetworkRetriever();
+        }
+
+        return TripsPeakNetworkRetriever.#instance;
+    }
+
+    async retrieve<ITrips>(routeId : string): Promise<ITrips | undefined> {
+        if (!TripsPeakNetworkRetriever.#trips) {
+            TripsPeakNetworkRetriever.#trips = await fetch(`${this.API_URL}route2${this.API_URL_END}`)
+            .then((response : Response) => 
+                response.ok ? 
+                response.json().then((json : any) => json.routes) 
+                : undefined
+            );
+        }
+
+        return TripsPeakNetworkRetriever.#trips?.filter((trip) => trip.routeID.toString() === routeId) as ITrips; 
+    }
+}
+
+class ShapesPeakNetworkRetriever extends PeakNetworkRetriever {
+    static #instance: ShapesPeakNetworkRetriever;
+    static #shapes: Array<any> | undefined;
+    
+    private constructor() {super();}
+
+    public static get instance(): ShapesPeakNetworkRetriever {
+        if (!ShapesPeakNetworkRetriever.#instance) {
+            ShapesPeakNetworkRetriever.#instance = new this();
+        }
+
+        return ShapesPeakNetworkRetriever.#instance;
+    }
+
+    async retrieve<IShapes>(shapeId : string): Promise<IShapes | undefined> {
+        if (!ShapesPeakNetworkRetriever.#shapes) {
+            ShapesPeakNetworkRetriever.#shapes = await fetch(`${this.API_URL}shape2${this.API_URL_END}`)
+            .then((response : Response) => 
+                response.ok ? 
+                response.json().then((json : any) => json.shape) 
+                : undefined
+            );
+        }
+
+        return ShapesPeakNetworkRetriever.#shapes?.filter((shape) => shape.shapeID.toString() === shapeId) as IShapes; 
     }
 }
