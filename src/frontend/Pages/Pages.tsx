338d4316d0e6968822e9166a662c54a66b861f78
Adam Yaj <86120028+CubeToPlay@users.noreply.github.com>
2025-04-17 22:19:29 -0500

    Remove redundant import of useState and useEffect from Pages.tsx

--- src/frontend/Pages/Pages.tsx
+++ src/frontend/Pages/Pages.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Map from "src/frontend/Pages/Map/Map";
 import About from "src/frontend/Pages/About/About";
 import Schedules from "src/frontend/Pages/Schedule/Schedules";
